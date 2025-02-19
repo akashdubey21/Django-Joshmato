7f56ed94d2779d908238cf5ea5839ac6d1528a07
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-20 00:00:55 +0600

    extended base.html to others html file and added necessary blocks

--- Base_App/urls.py
+++ Base_App/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
-from Base_App.views import Home_view
+from Base_App.views import Home_view,About_view,Menu_view,Book_table_view
 
 urlpatterns = [
     path('', Home_view,name="Home_view"),
+    path('about/', About_view,name="About_view"),
+    path('menu/', Menu_view,name="Menu_view"),
+    path('bookatable', Book_table_view,name="Book_table_view"),
     
 ]
--- Base_App/views.py
+++ Base_App/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def Home_view(request):
     return render(request, "home.html")
+
+
+def About_view(request):
+    return render(request, "about.html")
+
+
+def Menu_view(request):
+    return render(request, "menu.html")
+
+
+def Book_table_view(request):
+    return render(request, "book_table.html")
+
+
+
