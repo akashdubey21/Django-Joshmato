d887fa528ec837f99d7a6ea1039b4a38d102390b
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-20 00:51:12 +0600

    added database schema

--- Base_App/models.py
+++ Base_App/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class ItemList(models.Model):
+    Category_name = models.CharField(max_length=50)
+
+
+class Items(models.Model):
+    item_name = models.CharField(max_length=50)
+    description = models.TextField(blank=False)
+    price = models.IntegerField()
+    category = models.ForeignKey(ItemList,related_name="name",on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='Items/')
+    
+
+
+
+class AboutUs(models.Model):
+    description = models.TextField(blank=False)
+
+
+
+class Feedback(models.Model):
+    user_name = models.CharField(max_length=50)
+    description = models.TextField(blank=False)
+    rating = models.IntegerField()
+    
+
+
+class BookTable(models.Model):
+    user_name = models.CharField(max_length=50)
+    phone_number = models.IntegerField()
+    email = models.EmailField()
+    total_person = models.IntegerField()
+    booking_date = models.DateField()
